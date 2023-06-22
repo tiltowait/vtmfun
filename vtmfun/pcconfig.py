ce5defc3d43ca5ee0b98ef4dd89fdc4d906fe455
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-06-21 17:12:56 -0700

    Add bun_path

--- vtmfun/pcconfig.py
+++ vtmfun/pcconfig.py
@@ -3,6 +3,7 @@ import pynecone as pc
 config = pc.Config(
     app_name="vtmfun",
     api_url="https://vtmfun-production.up.railway.app:8000",
+    bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
 )
