cf41e5fe9e9b550343426803e0724545a0bac577
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-06-21 17:08:29 -0700

    Add Railway domain

--- vtmfun/pcconfig.py
+++ vtmfun/pcconfig.py
@@ -2,6 +2,7 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="vtmfun",
+    api_url="https://vtmfun-production.up.railway.app:8000",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
 )
