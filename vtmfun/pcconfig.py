bab59c9ab3d81f669b280ccb440d221bfadfd45a
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-06-21 17:20:27 -0700

    Update URL config

--- vtmfun/pcconfig.py
+++ vtmfun/pcconfig.py
@@ -1,8 +1,14 @@
+"""Pynecone app configuration."""
+
+import os
+
 import pynecone as pc
+
+PORT = os.getenv("PORT", 8000)
 
 config = pc.Config(
     app_name="vtmfun",
-    api_url="https://vtmfun-production.up.railway.app:8000",
+    api_url=f"https://0.0.0.0:{PORT}",
     bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
