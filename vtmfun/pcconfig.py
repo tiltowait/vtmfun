dc5cb26dd7be20f92a6920b688f53a27db331494
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-06-21 17:35:03 -0700

    Fix bun location for Docker

--- vtmfun/pcconfig.py
+++ vtmfun/pcconfig.py
@@ -7,9 +7,9 @@ import pynecone as pc
 PORT = os.getenv("PORT", 8000)
 
 config = pc.Config(
-    app_name="vtmfun",
-    api_url=f"https://0.0.0.0:{PORT}",
-    bun_path="$HOME/.bun/bin/bun",
+    app_name="app",
+    api_url=f"0.0.0.0:{PORT}",
+    bun_path="/app/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
 )
